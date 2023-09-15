0f0ae23f23d61be53f0c3121ac8db69e87f81615
yanlings <e0774088@u.nus.edu>
2023-09-15 14:48:42 +0800

    Edit code for App

---
+++ assignment/assignment1/assg1/src/AddQuestionForm.tsx
@@ -0,0 +1,95 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+
+interface FormData {
+  id: number;
+  title: string;
+  complexity: string;
+  category: string;
+  description: string;
+}
+
+interface AddQuestionFormProps {
+  onAddQuestion: (formData: FormData) => void;
+}
+
+const AddQuestionForm: React.FC<AddQuestionFormProps> = ({ onAddQuestion }) => {
+  const [formData, setFormData] = useState<FormData>({
+    id: -1,
+    title: '',
+    complexity: '',
+    category: '',
+    description: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // Call the onAddQuestion callback with the new question data
+    onAddQuestion(formData);
+    // Reset the form
+    setFormData({
+      id: -1,
+      title: '',
+      complexity: '',
+      category: '',
+      description: '',
+    });
+  };
+
+  return (
+    <div className="add-question-form">
+      <h2>Add a New Question</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Complexity</label>
+          <input
+            type="text"
+            name="complexity"
+            value={formData.complexity}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Category</label>
+          <input
+            type="text"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Description</label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Add Question</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddQuestionForm;
