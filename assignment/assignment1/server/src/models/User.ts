dbf2dc27564aedb31e9c00b72ed84b9e6163298e
Elizabeth Hong <lizziehong.sg@gmail.com>
2023-09-18 01:18:54 +0800

    create user and question models

---
+++ assignment/assignment1/server/src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 2, 
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    questions: {
+      type: Array,
+      default: [],
+    },
+  }, {timestamps: true}
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
