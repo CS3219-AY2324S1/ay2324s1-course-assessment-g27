56155ce4d8da956a8694c20ca915b56f3349c00b
Elizabeth Hong <lizziehong.sg@gmail.com>
2023-11-04 01:06:03 +0800

    attempt at dockerising front-end

--- project/front-end/src/App.tsx
+++ project/front-end/src/App.tsx
@@ -1,125 +1,3 @@
-// import React, { useState } from "react";
-// import LoginPage from "./scenes/loginPage/LoginPage";
-// import QuestionPage from "./scenes/questionPage/QuestionPage";
-// import "./App.css";
-
-// interface User {
-//   username: string;
-//   password: string;
-// }
-
-// interface Question {
-//   index: string;
-//   title: string;
-//   description: string;
-//   difficulty: string;
-//   tags: Array<string>;
-//   examples: Array<string>;
-//   constraints: Array<string>;
-// }
-
-// function App() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const [currentUsername, setCurrentUsername] = useState<string | null>(null);
-//   const [loginError, setLoginError] = useState<string | string>("");
-//   const [signUpError, setSignUpError] = useState<string | string>("");
-
-//   // Sample questions data
-//   const questions: Question[] = [
-//     {
-//       index: "1",
-//       title: "Reverse a String",
-//       description: "Write a function to reverse a given string.",
-//       difficulty: "Easy",
-//       tags: ["Strings", " Algorithms"],
-//       examples: ["Input: 'hello', Output: 'olleh'"],
-//       constraints: ["Do not use built-in reverse methods."],
-//     },
-//     {
-//       index: "2",
-//       title: "Linked List Cycle Detection",
-//       description: "Detect if a linked list contains a cycle.",
-//       difficulty: "Medium",
-//       tags: ["Data Structures", "Algorithms"],
-//       examples: ["Input: [1 -> 2 -> 3 -> 4 -> 2], Output: true"],
-//       constraints: ["You must use constant space."],
-//     },
-//     {
-//       index: "3",
-//       title: "Roman to Integer",
-//       description: "Convert a Roman numeral to an integer.",
-//       difficulty: "Easy",
-//       tags: ["Algorithms"],
-//       examples: ["Input: 'III', Output: 3"],
-//       constraints: ["Roman numerals are given in the range of 1 to 3999."],
-//     },
-//     // Add more questions here...
-//   ];
-
-//   // Sample user data
-//   const database: User[] = [
-//     {
-//       username: "user1",
-//       password: "pass1",
-//     },
-//     {
-//       username: "user2",
-//       password: "pass2",
-//     },
-//     // Add more users here...
-//   ];
-
-//   const handleLogin = (username: string, password: string) => {
-//     const userData = database.find((user) => user.username === username);
-
-//     if (userData && userData.password === password) {
-//       setIsLoggedIn(true);
-//       setCurrentUsername(username);
-//       setLoginError(""); // Reset login error
-//     } else {
-//       setLoginError("Invalid username or password"); // Set login error
-//     }
-//   };
-
-//   const handleSignUp = (username: string, password: string) => {
-//     // Check if the username is already taken
-//     if (database.some((user) => user.username === username)) {
-//       setSignUpError("Username is already taken. Please choose another.");
-//     } else {
-//       // Add the new user to the list
-//       database.push({ username, password });
-//       setIsLoggedIn(true);
-//       setCurrentUsername(username);
-//       setSignUpError(""); // Reset signup error
-//     }
-//   };
-
-//   return (
-//     <div className="app">
-//       <div className="login-form">
-//         {isLoggedIn ? (
-//           <div className="question-page">
-//             {currentUsername && <h1>Welcome, {currentUsername}!</h1>}
-//             <QuestionPage questions={questions} />
-//           </div>
-//         ) : (
-//           <div>
-//             <LoginPage
-//               onLogin={handleLogin}
-//               onSignUp={handleSignUp}
-//               errorMessages={{ login: loginError, signUp: signUpError }}
-//             />
-//             {loginError && <p className="error-message">{loginError}</p>}
-//             {signUpError && <p className="error-message">{signUpError}</p>}
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./scenes/loginPage/LoginPage";
 import QuestionPage from "./scenes/questionPage/QuestionPage";
@@ -149,25 +27,22 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-          <Route path="/" element={isAuth ?<Navigate to="/homePage" /> : <LoginPage />} />
+            <Route
+              path="/"
+              element={isAuth ? <Navigate to="/homePage" /> : <LoginPage />}
+            />
             <Route
               path="/homePage"
-              element={isAuth ? <HomePage/> : <Navigate to="/" />}
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
             />
-            <Route path="/questions" element={<QuestionPage/>} />
-            <Route path="/roompage/:roomid" element={isAuth ? <RoomPage /> : <Navigate to="/"/>} />
+            <Route path="/questions" element={<QuestionPage />} />
             <Route
-              path="/profile/:activePage"
-              element={<ProfilePage />}
+              path="/roompage/:roomid"
+              element={isAuth ? <RoomPage /> : <Navigate to="/" />}
             />
-            <Route
-              path="/password"
-              element={<ChangePwdPage />}
-            />
-            <Route
-              path="/history"
-              element={<QnsHistPage />}
-            />
+            <Route path="/profile/:activePage" element={<ProfilePage />} />
+            <Route path="/password" element={<ChangePwdPage />} />
+            <Route path="/history" element={<QnsHistPage />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
