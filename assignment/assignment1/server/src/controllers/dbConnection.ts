8204f7cec32dcf7972257b554d623152b0a81fee
Benny <benny.ong348@gmail.com>
2023-11-13 00:07:37 +0800

    Added save user attempt functionality
    -Updated sql databse
    -Changed 'Date' to 'Past attempts' and its now clickable to see a popup of the past attempt

--- assignment/assignment1/server/src/controllers/dbConnection.ts
+++ assignment/assignment1/server/src/controllers/dbConnection.ts
@@ -1,13 +1,26 @@
 import { Pool } from "pg";
+const POSTGRES_PASSWORD= `cs3219-g27`;
+const POSTGRES_DB= `postgres`;
+const POSTGRES_USER= `postgres`;
+const POSTGRES_HOST= `34.142.136.87`;
+const POSTGRES_PORT= 5432;
 
 /**
  * Connection to your psql database 
  * (using local db for now)
  */
+// export const pool = new Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "postgres",
+//     password: "postgres",
+//     port: 5432,
+// });
+
 export const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "postgres",
-    port: 5432,
-});
+    user: POSTGRES_USER,
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    password: POSTGRES_PASSWORD,
+    port: POSTGRES_PORT,
+  });
