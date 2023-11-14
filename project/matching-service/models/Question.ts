10fb7b2b237fc3eebe14f315f11ea69b66a5f890
junjunjieOng <e0544320@u.nus.edu>
2023-11-15 00:55:10 +0800

    change on question model

--- project/matching-service/models/Question.ts
+++ project/matching-service/models/Question.ts
@@ -1,44 +1,3 @@
-import mongoose from "mongoose";
-
-const questionSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    difficulty: {
-      type: String,
-      required: true
-    },
-    description: {
-      type: String,
-      required: true
-    },
-    examples: {
-      type: Array,
-      default: []
-    },
-    constraints: {
-      type: Array,
-      default: []
-    },
-    tags: {
-      type: String,
-    },
-    // picturePath: String,
-    // likes: {
-    //   type: Number,
-    //   default: 0
-    // },
-    // dislikes: {
-    //   type: Number,
-    //   default: 0
-    // },
-  }, 
-  { timestamps: true}
-);
-
 export type QuestionModel = {
   _id: string,
   title: string,
@@ -55,6 +14,3 @@ type Example = {
   explanation:string,
   image:string,
 }
-
-const Question = mongoose.model("Question", questionSchema);
-export default Question;
