4f95b62dd9510a7df13d89c444f896efbeac5df8
Benny <misterrobawt@gmail.com>
2023-11-18 01:34:11 +0800

    Update dbSeed.ts to import from dbConnection

--- assignment/server/src/dbSeed.ts
+++ assignment/server/src/dbSeed.ts
@@ -1,20 +1,6 @@
 import { Pool } from "pg";
 import { db } from "./sqlDb";
-
-/**
- * Connection to your psql database 
- * (using local db for now)
- */
-
-//change the fields as you need, but this should be the default
-export const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "postgres",
-    port: 5432,
-});
-
+import { pool } from "./controllers/dbConnection";
 
 export async function seedDb() {
     const conn = await pool.connect();
